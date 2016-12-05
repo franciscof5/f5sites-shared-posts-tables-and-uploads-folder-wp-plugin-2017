220c2b17419f609f877237f415278f8afc3beb05
Francisco Mat <francisco@f5sites.com>
2016-12-05 17:26:00 -0300

    Shared upload dir. Shared posts: after tests the multiste did not worked well, trick must be made in wp-db.php file directly

--- shared-wp_posts.php
+++ shared-wp_posts.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Shared wp_posts
  * Plugin URI: http://f5sites.com/shared-wp_posts
- * Description: Shared wp posts, for use in fnetwork
+ * Description: Shared wp posts, for use in fnetwork. (not working in wp-mu, trick must be made, end of file))
  * Version: 0.1
  * Author: Francisco Matelli Matulovic
  * Author URI: http://franciscomat.com
@@ -33,26 +33,60 @@ function shared_for_real($query) {
 	$wpdb->posts="f5sites_posts";
 	$wpdb->postmeta="f5sites_postmeta";
 
-	#$wpdb->categories="f5sites_categories";
-	#$wpdb->term_post2cat="f5sites_post2cat";
+	$wpdb->categories="f5sites_categories";
+	$wpdb->term_post2cat="f5sites_post2cat";
 
 	$wpdb->terms="f5sites_terms";
 	$wpdb->term_relationships="f5sites_term_relationships";
 	$wpdb->term_taxonomy="f5sites_term_taxonomy";
 	$wpdb->taxonomy="f5sites_taxonomy";
 	$wpdb->termmeta="f5sites_termmeta";
-	
+
 	if ( $query->is_home ) {
 		$domain=$_SERVER['HTTP_HOST'];
 		$idObj = get_category_by_slug($domain); 
 		$id = $idObj->term_id;
 		
-		$type=$query->query["post_type"];
-		
-		if($type==NULL)
+		#$type=$query->query["post_type"];
+		#if(!isset($type))
+		#if($type==NULL)
+		if(!isset($query->query["post_type"]))
 		$query->set( 'cat', $id );
 	}
 }
 
+
+function shared_upload_dir( $dirs ) {
+    $dirs['baseurl'] = network_site_url( '/wp-content/uploads/fnetwork' );
+    $dirs['basedir'] = ABSPATH . 'wp-content/uploads/fnetwork';
+    $dirs['path'] = $dirs['basedir'] . $dirs['subdir'];
+    $dirs['url'] = $dirs['baseurl'] . $dirs['subdir'];
+
+    return $dirs;
+}
+
+add_filter( 'upload_dir', 'shared_upload_dir' );
+
+/*
+THE TRICK FOR MULTISTE
+you must change wp-db.php file for change all queries before anything, replacing content on line 1055 (please suggest an improvment)
+
+wp-db.php
+********************************** REPLACE THAT **********************************************
+1059				else
+1060					$tables[ $table ] = $blog_prefix . $table;
+
+********************************** WITH THAT **********************************************
+				else {	
+					#TODO: F5SITES AQUI PASSAR PRA PLUGIN 
+					#if(in_array($domain, FNETWORK)
+					$SHARED_TABLES_NO_PREFIXED = array( 'posts', 'postmeta', 'comments', 'links', 'postmeta', 'terms', 'term_taxonomy', 'term_relationships', 'termmeta', 'commentmeta' );
+					if (in_array( $table, $SHARED_TABLES_NO_PREFIXED))
+					$blog_prefix = "f5sites_";
+					else
+					$blog_prefix = $this->get_blog_prefix( $blog_id );#$GLOBALS['domain_table_prefix'];
+					$tables[ $table ] = $blog_prefix . $table;
+				}
+*/
 ?>
 
