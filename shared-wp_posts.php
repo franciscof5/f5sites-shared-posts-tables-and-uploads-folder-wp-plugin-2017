d636aa3c591f6e396ed707954b3cf85c79f3f3f4
Francisco Mat <francisco@f5sites.com>
2016-12-07 19:58:08 -0300

    Plugin complete, no need for tricks anymore, used 2 hooks for bug fix. Pdfcatalog2 readyness

--- shared-wp_posts.php
+++ shared-wp_posts.php
@@ -17,42 +17,79 @@
 
 #add_filter('query', 'use_f5sites_posts');
 #add_action( 'plugins_loaded', 'shared_for_real' );
+
 add_action( 'pre_get_posts', 'shared_for_real' );
+
+add_action( 'plugins_loaded', 'shared_for_real' );
+
 #add_action( 'parse_site_query', 'shared_for_real' );
 #add_action( "init", "shared_for_real" );
 #add_action("admin-init", "shared_for_real");
 #shared_for_real();
 
 function shared_for_real($query) {
-	#var_dump($query);
+	if(isset($query->query["post_type"]))
+	$type = $query->query["post_type"];
+	#
+	$domain=$_SERVER['HTTP_HOST'];
+	$idObj = get_category_by_slug($domain);
+	
+	if(isset($idObj->term_id))
+	$id = $idObj->term_id;
+	#
+	$is_pdf_catalog = isset($_GET["pdfcat"]);
+	$is_pdf_catalog_all = isset($_GET["all"]);
+	$is_defined_post_type = isset($type);
+	
+	#if(!isset($id))
+	#$id = 357;
+	
+	#
+	#echo "type ".$type;
+	#$query->query_vars["category__in"] = $id;
+	#$query->query_vars["category__in"] = 357;
+	#var_dump("tyyyyype: ".$type. " is_shop: ".is_shop(). "domain: ".$domain. " is_woocommerce(): ".is_woocommerce(). "?pdfcat: ".$is_pdf_catalog. " gettype: ".gettype($query));
+	#var_dump($query); #&& !is_pdf_catalog
+	if(!$is_defined_post_type || $type=="product") { # && !is_pdf_catalog
+		if(gettype($query)!="string") {
+			if(is_woocommerce() || $is_pdf_catalog) { # || $is_pdf_catalog
+				if(!$is_pdf_catalog_all)
+				$query->set( 'product_cat', $domain );
+			} else {
+				if(isset($id))
+				$query->set( 'cat', $id );
+			}
+		}
+	}
+	
 	global $wpdb;
-
+	
+	$wpdb->posts="f5sites_posts";
+	$wpdb->postmeta="f5sites_postmeta";
 	$wpdb->comments="f5sites_comments";
 	$wpdb->commentmeta="f5sites_commentmeta";
 	$wpdb->links="f5sites_links";
-	$wpdb->posts="f5sites_posts";
-	$wpdb->postmeta="f5sites_postmeta";
-
+	
+	$wpdb->terms="f5sites_terms";
+	$wpdb->term_taxonomy="f5sites_term_taxonomy";
+	$wpdb->term_relationships="f5sites_term_relationships";
+	$wpdb->termmeta="f5sites_termmeta";
+	$wpdb->taxonomy="f5sites_taxonomy";
+	
 	$wpdb->categories="f5sites_categories";
 	$wpdb->term_post2cat="f5sites_post2cat";
 
-	$wpdb->terms="f5sites_terms";
-	$wpdb->term_relationships="f5sites_term_relationships";
-	$wpdb->term_taxonomy="f5sites_term_taxonomy";
-	$wpdb->taxonomy="f5sites_taxonomy";
-	$wpdb->termmeta="f5sites_termmeta";
-
-	if ( $query->is_home ) {
-		$domain=$_SERVER['HTTP_HOST'];
-		$idObj = get_category_by_slug($domain); 
-		$id = $idObj->term_id;
+	#if ( $query->is_home ) {
+	#product, shop_order, shop_coupon
+	
 		
+		
+		#woocommerce
 		#$type=$query->query["post_type"];
 		#if(!isset($type))
 		#if($type==NULL)
-		if(!isset($query->query["post_type"]))
-		$query->set( 'cat', $id );
-	}
+
+	#}
 }
 
 
@@ -68,6 +105,7 @@ function shared_upload_dir( $dirs ) {
 add_filter( 'upload_dir', 'shared_upload_dir' );
 
 /*
+NO NEED FOR TRICK ANYMORE
 THE TRICK FOR MULTISTE
 you must change wp-db.php file for change all queries before anything, replacing content on line 1055 (please suggest an improvment)
 
